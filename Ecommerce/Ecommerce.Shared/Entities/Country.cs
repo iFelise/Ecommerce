277d251ccc8d583ff2ea5ee4792bb3d2af1a7ec2
iFelise <crespofelipe999@gmail.com>
2025-05-06 17:19:10 -0500

    NewStructuredAndCountryEntity

---
+++ Ecommerce/Ecommerce.Shared/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage = "El campo {0} Es obligatorio")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
